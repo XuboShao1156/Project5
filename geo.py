0319de0c65dbadd2b023af20dcc1b7398808dc89
Kezhi Xiong <august.xkz@gmail.com>
2022-04-25 20:47:42 -0400

    Build geo data for cloud providers.

--- geo.py
+++ geo.py
@@ -1,13 +1,20 @@
-import socket
+import csv
+import ipaddress
+import time
 import requests
 
 
 def query(ip) -> (int, int):
-    return _query_by_api(ip)
+    lat, lon = _query_by_db(ip)
+    if lat is None or lon is None:
+        lat, lon = _query_by_api(ip)
+    return lat, lon
 
 
 session = requests.session()
+session.get('https://geolite.info')
 def _query_by_api(ip) -> (int, int):
+    print('query api...')
     resp = session.get('https://geolite.info/geoip/v2.1/city/' + ip, auth=('708001', 'RRX49qWHqHjWSrQq'))
     if resp.status_code / 100 != 2:
         raise Exception('ip location query not succeed!')
@@ -16,15 +23,36 @@ def _query_by_api(ip) -> (int, int):
     return round(body['location']['latitude']), round(body['location']['longitude'])
 
 
-def _query_by_db(ip) -> (int, int):
-    pass
+def _build_geo_db():
+    d = dict()
+    with open('geo_db.csv', 'r') as f:
+        reader = csv.DictReader(f)
+        for row in reader:
+            ip_int = int(ipaddress.ip_address(row['network'].split('/')[0]))
+            d[ip_int] = (row['network'], int(row['latitude']), int(row['longitude']))
+    return d
+
+
+db = _build_geo_db()
+mask = 0x1
+for i in range(32):
+    mask = mask << 1 | 0x1
+def _query_by_db(ip, mask=mask) -> (int, int):
+    print('query db...')
+    ip = int(ipaddress.ip_address(ip))
+    while ip != 0:
+        ip &= mask
+        if ip in db:
+            return db[ip][1], db[ip][2]
+        mask <<= 1
+    return None, None
 
 
 if __name__ == '__main__':
-    servers = []
-    with open('replica_server', 'r') as f:
-        for host in f.readlines():
-            host = host.rstrip('\n')
-            ip = socket.gethostbyname(host)
-            servers.append([host, ip, query(ip)])
-    print(servers)
+    start = time.time()
+    print('query by db: ' + str(_query_by_db('54.251.196.47')))
+    print(time.time() - start)
+
+    start = time.time()
+    print('query by api: ' + str(_query_by_api('54.251.196.47')))
+    print(time.time() - start)
--- mapping.py
+++ mapping.py
@@ -1,3 +1,6 @@
+import socket
+import time
+
 from geo import query
 from math import sin, cos, sqrt, atan2, radians
 
@@ -9,10 +12,17 @@ replicas = [['p5-http-a.5700.network', '50.116.41.109', (34, -84)],
             ['p5-http-f.5700.network', '88.80.186.80', (52, 0)],
             ['p5-http-g.5700.network', '172.105.55.115', (19, 73)]]
 
+cache = dict()
 
 def map_replica(ip) -> str:
-    lat, lon = query(ip)
+    if ip in cache:
+        return cache[ip]
 
+    start = time.time()
+    lat, lon = query(ip)
+    print('ip geo query: ' + str(time.time() - start))
+
+    start = time.time()
     best = 0
     min_dist = 1_000_000_000
     for idx, rep in enumerate(replicas):
@@ -20,8 +30,10 @@ def map_replica(ip) -> str:
         if dist < min_dist:
             min_dist = dist
             best = idx
+    print('compute nearest replica: ' + str(time.time() - start))
 
-    return replicas[best][1]
+    cache[ip] = replicas[best][1]
+    return cache[ip]
 
 
 def _distance(lat1, lon1, lat2, lon2) -> float:
@@ -35,3 +47,17 @@ def _distance(lat1, lon1, lat2, lon2) -> float:
     a = sin(dlat / 2) ** 2 + cos(lat1) * cos(lat2) * sin(dlon / 2) ** 2
     c = 2 * atan2(sqrt(a), sqrt(1 - a))
     return 6373 * c
+
+
+def _query_replicas():
+    servers = []
+    with open('replica_server', 'r') as f:
+        for host in f.readlines():
+            host = host.rstrip('\n')
+            ip = socket.gethostbyname(host)
+            servers.append([host, ip, query(ip)])
+
+
+if __name__ == '__main__':
+    print(map_replica('54.215.100.111'))
+
