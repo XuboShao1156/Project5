0f0c10b140addfb64ae461cfb5dbae2628599768
Kezhi Xiong <august.xkz@gmail.com>
2022-04-30 21:34:03 -0400

    Send req to private geo service.

--- geo.py
+++ geo.py
@@ -3,24 +3,11 @@ import ipaddress
 import time
 import requests
 
-
 def query(ip) -> (int, int):
     lat, lon = _query_by_db(ip)
     if lat is None or lon is None:
-        lat, lon = _query_by_api(ip)
+        lat, lon = _query_external_api(ip)
     return lat, lon
-
-
-session = requests.session()
-session.get('https://geolite.info')
-def _query_by_api(ip) -> (int, int):
-    print('query api...')
-    resp = session.get('https://geolite.info/geoip/v2.1/city/' + ip, auth=('708001', 'RRX49qWHqHjWSrQq'))
-    if resp.status_code / 100 != 2:
-        raise Exception('ip location query not succeed!')
-
-    body = resp.json()
-    return round(body['location']['latitude']), round(body['location']['longitude'])
 
 
 def _build_geo_db():
@@ -46,6 +33,30 @@ def _query_by_db(ip, mask=mask) -> (int, int):
             return db[ip][1], db[ip][2]
         mask <<= 1
     return None, None
+
+
+external_session = requests.session()
+external_session.get('https://geolite.info')
+def _query_external_api(ip) -> (int, int):
+    resp = external_session.get('https://geolite.info/geoip/v2.1/city/' + ip, auth=('708001', 'RRX49qWHqHjWSrQq'))
+    if resp.status_code / 100 != 2:
+        raise Exception('ip location query not succeed!')
+
+    body = resp.json()
+    print('query external api...')
+    return round(body['location']['latitude']), round(body['location']['longitude'])
+
+
+internal_session = requests.session()
+requests.get('http://45.33.89.80:40007/geoip/45.33.89.80')
+def _query_internal_api(ip) -> (int, int):
+    resp = internal_session.get('http://45.33.89.80:40007/geoip/' + ip)
+    if resp.status_code / 100 != 2:
+        raise Exception('ip location query not succeed!')
+
+    body = resp.json()
+    print('query internal api...')
+    return round(body['lat']), round(body['lon'])
 
 
 if __name__ == '__main__':
