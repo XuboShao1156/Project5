fa32e079d392c1b355f933a79e37fd2f80fac9dc
Kezhi Xiong <august.xkz@gmail.com>
2022-05-02 00:33:34 -0400

    Seperate cache into a independent file.

--- httpserver_test.py
+++ httpserver_test.py
@@ -1,11 +1,11 @@
 import csv
 import requests
 
-replica = 'p5-http-a.5700.network'
+replica = 'p5-http-a.5700.network',
 
 session = requests.session()
 with open('pageviews.csv') as f:
     for row in csv.reader(f):
-        if int(row[1]) > 6000:
-            resp = session.get('http://{}:40007/Main_Page'.format(replica))
+        if int(row[1]) > 0:
+            resp = session.get('http://{}:40007/{}'.format(replica, row[0]))
             print("{} for {}".format(resp.status_code, row[0]))
